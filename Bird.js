5a8fd879d5f3f695793665b145d27da93ae4a4b2
JuhiPendse <juhipendse08@gmail.com>
2021-03-02 18:49:01 +0530

    done

--- Bird.js
+++ Bird.js
@@ -9,7 +9,7 @@ class Bird extends BaseClass {
   display() {
     //this.body.position.x = mouseX;
     //this.body.position.y = mouseY;
-
+ 
     super.display();
 
     if(this.body.velocity.x > 10 && this.body.position.x > 200){
@@ -20,6 +20,7 @@ class Bird extends BaseClass {
 
     for(var i=0; i<this.trajectory.length; i++){
       image(this.smokeImage, this.trajectory[i][0], this.trajectory[i][1]);
+      
     }
   }
 }
